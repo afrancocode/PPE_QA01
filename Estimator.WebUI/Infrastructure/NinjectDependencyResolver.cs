6d555713c6c145e8536a714d070d35f8bfcb1806
dmendezv <dmendez@codeservices.com.mx>
2015-07-13 15:41:11 -0700

    Estimator Project Begins
    Estimator Web

---
+++ Estimator.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,39 @@
+﻿using Estimator.Model.Entities;
+using Estimator.Model.Abstract;
+using Estimator.Model.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Ninject;
+using System.Web.Mvc;
+
+namespace Estimator.WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+
+
+        private void AddBindings()
+        {
+            kernel.Bind<IUserRepository>().To<UserRepository>();
+        }
+    }
+}
