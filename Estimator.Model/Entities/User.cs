aed1cecb7b343e5a4ac8d14f210773520d9f5884
Dgescalante <descalante@codeservices.com.mx>
2015-07-15 15:00:35 -0700

    Login implementation (WIP)
    -Added Login view.
    -Modifications to Layout.
    -Added some attributes to User class.

--- Estimator.Model/Entities/User.cs
+++ Estimator.Model/Entities/User.cs
@@ -3,15 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 
 namespace Estimator.Model.Entities
 {
     public class User
     {
         public int Id { get; set; }
+		[Required(ErrorMessage="Please provide a valid Login", AllowEmptyStrings=false)]
+		[Display(Name="User Name:")]
         public string Login { get; set; }
         public string Name { get; set; }
+		[Required(ErrorMessage="Please provide password",AllowEmptyStrings=false)]
+		[DataType(DataType.Password)]
         public string Password { get; set; }
+		[HiddenInput(DisplayValue=false)]
         public string Role { get; set; }
     }
 }
