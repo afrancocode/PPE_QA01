e06f7e1c0c5ee2e49d0d18d6fcc03a9c28b9aa53
Dgescalante <descalante@codeservices.com.mx>
2015-07-15 08:03:30 -0700

    Missing register service of NinjectDependencyResolver

--- Estimator.WebUI/App_Start/NinjectWebCommon.cs
+++ Estimator.WebUI/App_Start/NinjectWebCommon.cs
@@ -61,6 +61,7 @@ namespace Estimator.WebUI.App_Start
         /// <param name="kernel">The kernel.</param>
         private static void RegisterServices(IKernel kernel)
         {
+			System.Web.Mvc.DependencyResolver.SetResolver(new Estimator.WebUI.Infrastructure.NinjectDependencyResolver(kernel));
         }        
     }
 }
