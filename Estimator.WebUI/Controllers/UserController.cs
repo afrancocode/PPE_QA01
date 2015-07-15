70aa1c65c6b5632057bc332d6ac542ddd5ecaa67
dmendezv <dmendez@codeservices.com.mx>
2015-07-15 09:44:13 -0700

    Changed variable names, adding User View and adding ErrorStyles.css file
    Changed variable names, adding User View and adding ErrorStyles.css file

--- Estimator.WebUI/Controllers/UserController.cs
+++ Estimator.WebUI/Controllers/UserController.cs
@@ -1,19 +1,27 @@
-﻿using System.Web.Mvc;
+﻿using System.Linq;
+using System.Web.Mvc;
 using Estimator.Model.Abstract;
-
+using Estimator.Model.Entities;
 
 namespace Estimator.WebUI.Controllers
 {
-    public class UserController : Controller
-    {
-        private IUserRepository Userrepository;
+	public class UserController : Controller
+	{
+		private IUserRepository userCollection;
 
-        public UserController(IUserRepository userRepository) {
-            this.Userrepository = userRepository;
-        }
+		public UserController(IUserRepository userRepository)
+		{
+			this.userCollection = userRepository;
+		}
 
-        public ViewResult UserList() {
-            return View(Userrepository.Users);
+		public ViewResult UserList()
+		{
+			return View(userCollection.Users);
+		}
+        public ViewResult Edit(int UserId)
+        {
+            User user = userCollection.Users.FirstOrDefault(p => p.Id == UserId);
+            return View(user);
         }
-    }
+	}
 }
