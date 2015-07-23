fa54acb96bbdb7d18827774d5c203a941dc45a87
linaborrego <lborrego@codeservices.com.mx>
2015-07-23 12:58:08 -0700

    Add View Model to Project

--- Estimator.WebUI/Models/LoginViewModel.cs
+++ Estimator.WebUI/Models/LoginViewModel.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Estimator.WebUI.Models
 {
 	public class LoginViewModel
 	{
+		[Required(ErrorMessage = "Please provide a valid Login", AllowEmptyStrings = false)]
+		[Display(Name = "User Name: ")]
+		public string Login { get; set; }
+
+		[Required(ErrorMessage = "Please provide a password", AllowEmptyStrings = false)]
+		[DataType(DataType.Password)]
+		public string Password { get; set; }
 	}
 }
