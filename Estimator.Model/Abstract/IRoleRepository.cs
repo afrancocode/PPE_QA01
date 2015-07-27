5490fa246b0d238485c956e5d855a51c3c956382
dmendezv <dmendez@codeservices.com.mx>
2015-07-27 16:30:58 -0700

    Separate Roles from PPDBDummyContext
    Separate Roles from PPDBDummyContext

---
+++ Estimator.Model/Abstract/IRoleRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Estimator.Model.Entities;
+
+namespace Estimator.Model.Abstract
+{
+	interface IRoleRepository
+	{
+		IEnumerable<Role> Role { get; }
+	}
+}
---
+++ Estimator.Repository/Concrete/PPRoleDummyContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Estimator.Model.Entities;
+
+namespace Estimator.Repository.Concrete
+{
+	public class PPRoleDummyContext
+	{
+		public List<Role> Roles = new List<Role>(){
+			Pricer,
+			Estimator
+		};
+
+		internal static Role Pricer = new Role { Id = 1, Name = "Pricer" };
+		internal static Role Estimator = new Role { Id = 2, Name = "Estimator" };
+	}
+}
